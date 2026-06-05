622b5813e6d3a0ba7efcc7c67209a0269ce067c2
Andre Gil <thomasgil913@gmail.com>
2026-06-05 00:10:40 -0500

    Created connection to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const rabbitURL = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(rabbitURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	log.Println("connection to broker was successful")
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	<-signalCh
+	log.Println("shutting down")
 }
